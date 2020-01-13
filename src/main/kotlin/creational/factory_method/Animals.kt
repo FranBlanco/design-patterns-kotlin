d0abad86c342f18467852454660a134be7081e10
fblanco <fblanco@tuenti.com>
2020-01-13 13:27:41 +0100

    Add factory method with a factory inside

---
+++ src/main/kotlin/creational/factory_method/Animals.kt
@@ -0,0 +1,34 @@
+package creational.factory_method
+
+import kotlin.random.Random
+
+interface Animal {
+    val name: String
+}
+
+class Cat : Animal {
+    override val name = "Cat"
+}
+
+class Dog(race: DogRaces) : Animal {
+    override val name = "Dog"
+
+}
+
+class AnimalFactory {
+    val dogFactory = DogFactory()
+    fun createAnimal(animalType: String): Animal {
+        return when (animalType.trim().toLowerCase()) {
+            "cat" -> Cat()
+            "dog" -> dogFactory.createDog()
+            else -> throw RuntimeException("Unknown animal $animalType")
+        }
+    }
+}
+
+class DogFactory{
+    fun createDog() = Dog(DogRaces.values()[Random.nextInt(DogRaces.values().size)])
+}
+enum class DogRaces {
+    DOBERMAN, MIXED, SHEPHERD, TERRIER
+}
