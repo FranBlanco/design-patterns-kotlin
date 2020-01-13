00cc1313bc8365fde30031a2dcfd1aeda2f433bf
fblanco <fblanco@tuenti.com>
2020-01-13 16:15:35 +0100

    Add Builder Pattern

---
+++ src/main/kotlin/creational/builder/Email.kt
@@ -0,0 +1,36 @@
+package creational.builder
+
+data class Mail(val to: String,
+                var title: String = "",
+                var message: String = "",
+                var cc: List<String> = listOf(),
+                var bcc: List<String> = listOf(),
+                var attachments: List<java.io.File> = listOf())
+
+class MailBuilder(private val to: String) {
+    private var mail: Mail = Mail(to)
+    fun title(title: String): MailBuilder {
+        mail.title = title
+        return this
+    }
+    fun message(message: String): MailBuilder {
+        mail.message = message
+        return this
+    }
+    fun cc(cc: List<String>): MailBuilder {
+        mail.cc = cc
+        return this
+    }
+    fun bcc(bcc: List<String>): MailBuilder {
+        mail.bcc = bcc
+        return this
+    }
+    fun attachments(attachments: List<java.io.File>): MailBuilder {
+        mail.attachments = attachments
+        return this
+    }
+
+    fun build(): Mail {
+        return mail
+    }
+}
